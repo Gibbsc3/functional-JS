30f3e4c5dcaca42729b470c04b80696f7fff6535
Tony <gibbsc@indiana.edu>
2016-07-01 14:21:11 -0400

    formatting

--- main.js
+++ main.js
@@ -1,57 +1,57 @@
 //********************NUMER 1**********
-var total = items.reduce(function(prevItem,currItem){
-  var prevPrice = isNaN(prevItem) ? prevItem.price : prevItem
-  // console.log(prevItem);
-  return prevPrice + currItem.price
+var total = items.reduce(function(prevItem, currItem) {
+    var prevPrice = isNaN(prevItem) ? prevItem.price : prevItem
+        // console.log(prevItem);
+    return prevPrice + currItem.price
 })
-console.log(total/items.length);
+console.log(total / items.length);
 //****************NUMBER1***********
 
 
 //****************NUMBER2 ** ** ** ** ** *
-    //sets the range for filter method
-    function priceRange(item) {
-        return item.price > 14 && item.price < 18;
-    }
-    //passes each item through and returns an array of OBJECTS with a price btwn 14 and 18
-    var titles = items.filter(priceRange);
-    //takes the filtered array objects and returns their titles
-    titles = titles.map(function(item) {
-        return 'TITLE:  ' + item.title
-    })
-    console.log(titles);
-    //*********************NUMBER2
+//sets the range for filter method
+function priceRange(item) {
+    return item.price > 14 && item.price < 18;
+}
+//passes each item through and returns an array of OBJECTS with a price btwn 14 and 18
+var titles = items.filter(priceRange);
+//takes the filtered array objects and returns their titles
+titles = titles.map(function(item) {
+    return 'TITLE:  ' + item.title
+})
+console.log(titles);
+//*********************NUMBER2
 
-    //*******************NUMBER3
-    function findCode(item) {
+//*******************NUMBER3
+function findCode(item) {
     return item.currency_code === 'GBP';
 }
-    console.log(items.find(findCode));
-    //***************NUMBER3
+console.log(items.find(findCode));
+//***************NUMBER3
 
-  //  *******************NUMBER4
-    function woodyOrNah(item) {
-      return item.materials.includes('wood');
-    }
-    var madeOfWood = items.filter(woodyOrNah)
-    console.log(madeOfWood);
-      //*******************NUMBER4
+//  *******************NUMBER4
+function woodyOrNah(item) {
+    return item.materials.includes('wood');
+}
+var madeOfWood = items.filter(woodyOrNah)
+console.log(madeOfWood);
+//*******************NUMBER4
 
-    //*******************NUMBER5
-    function aLotOmaterials(item) {
-     return item.materials.length > 8 ;
-    }
-     var madeWithLove = items.filter(aLotOmaterials)
-     console.log(madeWithLove);
-      //*******************NUMBER5
+//*******************NUMBER5
+function aLotOmaterials(item) {
+    return item.materials.length > 8;
+}
+var madeWithLove = items.filter(aLotOmaterials)
+console.log(madeWithLove);
+//*******************NUMBER5
 
-        //*******************NUMBER6
-        function madeBySeller(item) {
-         return item.who_made === "i_did";
-        }
-         var mamaImadeIt = items.filter(madeBySeller)
-         console.log(mamaImadeIt);
-          //*******************NUMBER6
+//*******************NUMBER6
+function madeBySeller(item) {
+    return item.who_made === "i_did";
+}
+var mamaImadeIt = items.filter(madeBySeller)
+console.log(mamaImadeIt);
+//*******************NUMBER6
 
 
 // var numbers = [1, 4, 9];
